5ba3311d38fe77936410c5a3d3d3e9bfa19c4465
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2024-03-16 09:35:57 +0800

    feat: install rename_admin_paths

---
+++ web/modules/contrib/rename_admin_paths/tests/src/Functional/AccessTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\Tests\rename_admin_paths\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * @group tests
+ */
+class AccessTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  protected static $modules = ['rename_admin_paths'];
+
+  /**
+   * Test that the admin is still protected after renaming it
+   */
+  public function testAdminNotAccessibleAfterRenaming() {
+    $output = $this->drupalGet('admin');
+    $this->assertStringContainsString('You are not authorized to access this page.', $output);
+
+    $output = $this->drupalGet('admin/modules');
+    $this->assertStringContainsString('You are not authorized to access this page.', $output);
+
+    $admin = $this->drupalCreateUser(['administer modules', 'administer site configuration'], 'administrator', TRUE);
+    $this->drupalLogin($admin);
+
+    $this->drupalGet('admin/config/system/rename-admin-paths');
+
+    $this->submitForm([
+      'admin_path' => 1,
+      'admin_path_value' => 'backend',
+      'user_path' => 0,
+      'user_path_value' => 'member',
+    ], 'Save configuration');
+
+    $this->drupalLogout();
+
+    $output = $this->drupalGet('backend');
+    $this->assertStringContainsString('You are not authorized to access this page.', $output);
+
+    $output = $this->drupalGet('backend/modules');
+    $this->assertStringContainsString('You are not authorized to access this page.', $output);
+  }
+
+}
---
+++ web/modules/contrib/rename_admin_paths/tests/src/Functional/AdminFormTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\Tests\rename_admin_paths\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * @group tests
+ */
+class AdminFormTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  protected static $modules = ['rename_admin_paths'];
+
+  /**
+   * Setup admin user.
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $admin = $this->drupalCreateUser(['administer modules', 'administer site configuration'], 'administrator', TRUE);
+    $this->drupalLogin($admin);
+  }
+
+  /**
+   * Test admin is able to view the settings form.
+   */
+  public function testViewForm() {
+    $this->drupalGet('admin/config/system/rename-admin-paths');
+
+    $this->assertRenameAdminPathFormIsVisible();
+
+    $this->assertSession()->fieldValueEquals('admin_path_value', 'backend');
+    $this->assertSession()->fieldValueEquals('user_path_value', 'member');
+  }
+
+  /**
+   * Test /admin and /user paths no longer exist when they are changed to /backend and /member
+   */
+  public function testEnablePathReplacements() {
+    $output = $this->drupalGet('user/1');
+    $this->assertStringContainsString('Member for', $output);
+
+    $this->drupalGet('admin/config/system/rename-admin-paths');
+
+    $this->submitForm([
+      'admin_path' => 1,
+      'admin_path_value' => 'backend',
+      'user_path' => 1,
+      'user_path_value' => 'member',
+    ], 'Save configuration');
+
+    $this->assertRenameAdminPathFormIsVisible();
+
+    $this->assertSession()->fieldValueEquals('admin_path_value', 'backend');
+    $this->assertSession()->fieldValueEquals('user_path_value', 'member');
+
+    $output = $this->drupalGet('user/1');
+    $this->assertStringContainsString('The requested page could not be found.', $output);
+
+    $output = $this->drupalGet('member/1');
+    $this->assertStringContainsString('Member for', $output);
+  }
+
+  private function assertRenameAdminPathFormIsVisible() {
+    $output = $this->getSession()->getPage()->getContent();
+    $this->assertStringContainsString('Rename admin path', $output);
+    $this->assertStringContainsString('Replace "admin" in admin path by', $output);
+    $this->assertStringContainsString('Rename user path', $output);
+    $this->assertStringContainsString('Replace "user" in user path by', $output);
+  }
+
+}
