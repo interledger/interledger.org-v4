972e5905c69eb7afa9ffff5212a95f177e694e8a
Stephan Butler <stephan@interledger.foundation>
2025-12-02 21:22:58 +0200

    fix(ci): rework staging build

--- ci/deploy/staging/cleanup.sh
+++ ci/deploy/staging/cleanup.sh
@@ -8,7 +8,7 @@ sudo chmod +x vendor/bin/drush.php
 sudo chmod +x vendor/drush/drush/drush
 
 # Also ensure www-data can read the entire vendor tree
-sudo chown -R www-data:www-data vendor
+sudo chown -R deployer:www-data vendor
 sudo chmod -R g+rX vendor
 
 # Test
