b253b0e153ca0ff1daf35b68a2f38d3833e67575
Sarah Jones <sarah38186@gmail.com>
2026-01-20 14:39:54 +0200

    chore: updated and installed composer deps (#190)

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '0acfc9ff704f67a8';
+  public const string VERSIONS_HASH = '542931196ee67cad';
 
 }
