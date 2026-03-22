ee8b85bd088222ac1464f9b347b4fe521b52c1e6
JoblersTune <sarah@interledger.org>
2026-03-22 14:54:37 +0200

    chore: updated Drupal core

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '542931196ee67cad';
+  public const string VERSIONS_HASH = '5e5c9ab18f282354';
 
 }
