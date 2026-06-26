a1a4a2a891edaeee262cc2567a651ac51e6c3837
Sarah Jones <sarah38186@gmail.com>
2026-06-26 13:09:31 +0200

    chore: upgraded drupal entity block and paragraph dependencies (#207)

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = 'f9a568dd5c93fb62';
+  public const string VERSIONS_HASH = '6efa20341134eb99';
 
 }
